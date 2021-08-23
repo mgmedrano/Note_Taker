a2730792fa550e02813676632187d7171d4b2acb
Michael Medrano <mgmedrano@outlook.com>
2021-08-22 17:40:32 -0700

    Additional Starter code added

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+//require
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
@@ -5,3 +6,15 @@ const fs = require('fs');
 let app = express ();
 let PORT = process.env.PORT || 3001;
 
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+app.use(express.static("public"));
+let notes = require("./db/db.json")
+
+app.get("/notes", function (req, res) {
+    res.sendFile(path.join(_dirname, "public/notes.html"));
+});
+
+app.listen(PORT, function () {
+    console.log("Listening on PORT" + PORT);
+});
