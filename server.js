4e179986be96fec4b1d5057820a340852e6a2039
Michael Medrano <mgmedrano@outlook.com>
2021-08-24 22:54:30 -0700

    init commit redo

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-//require
+//require node packages
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
@@ -14,6 +14,40 @@ let notes = require("./db/db.json")
 app.get("/notes", function (req, res) {
     res.sendFile(path.join(_dirname, "public/notes.html"));
 });
+
+app.get("/api/notes", function(req,res) {
+    fs.readFile("db/db.json", "utf8", function (err, data) {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        res.json(notes);
+    });
+});
+
+app.post("/api/notes", function (req, res) {
+    let note = String.fromCharCode(65 + Math.floor(Math.random() * 26));
+    let id = note + Date.now();
+    let newNote = {
+      id: id,
+      title: req.body.title,
+      text: req.body.text,
+    };
+    console.log(typeof notes);
+    notes.push(newNote);
+    const stringifyNote = JSON.stringify(notes);
+    res.json(notes);
+    fs.writeFile("db/db.json", stringifyNote, (err) => {
+      if (err) console.log(err);
+      else {
+        console.log("Note saved to db.json");
+      }
+    });
+  });
+
+  app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "public/index.html"));
+  });
 
 app.listen(PORT, function () {
     console.log("Listening on PORT" + PORT);
